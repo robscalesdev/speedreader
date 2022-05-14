4ff913d7decea830390ea8c85ee04858d164274d
rob <imafishyfriend@gmail.com>
2022-05-13 22:45:38 -0500

    updated style, basically a full refactor

--- src/Project/Home/Styles.js
+++ src/Project/Home/Styles.js
@@ -1,22 +1,38 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import {
+  BiPause,
+  BiPlay
+} from 'react-icons/bi';
+
+import {
+  FaStepBackward,
+  FaStepForward
+} from 'react-icons/fa'
+
+import {
+  RiRewindFill
+} from 'react-icons/ri'
 
 export const ProjectPage = styled.div`
   padding: 1rem 1.5rem;
   display: flex;
   flex-direction: column;
-  background-image: linear-gradient(100deg, #66f, #33f, #33f, #77f);
+  background: #FFEEE7;
 
   @media screen and (max-width: 1100px) {
     padding: 2% 3%;
   }
 `
 
+export const SubredditContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 export const SubredditWrapper = styled.div`
   display: flex; 
-  /* flex-direction: column; */
-  justify-content: flex-start; 
-  /* width: 30%; */
+  justify-content: flex-start;
 
   @media screen and (max-width: 600px) {
     flex-direction: row;
@@ -24,27 +40,151 @@ export const SubredditWrapper = styled.div`
   }
 `
 
-export const PostWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+export const SubredditButton = styled.button`
+  font-family: 'Merriweather Sans';
+  font-size: 0.75rem;
+  padding: 0 0.5rem;
+  height: 2rem;
+  margin: 0 0.25rem;
+  border-radius: 80px;
+  background: ${state => state.selected ? '#D02500' : '#6B1603'};
+  box-shadow: 0px 8px 24px rgba(208, 37, 0, 0.09);
+  border: none;
+  color: #fff;
 `
 
-export const Post = styled(Link)`
-  text-decoration: none;
-  padding: 1rem;
-  margin: 0.5rem 1rem;
-  font-size: 1.1rem;
-  letter-spacing: 0.5px;
-  border-style: solid;
-  border-width: 1px;
-  border-color: gray;
-  border-radius: 3px;
-  color: #222;
-  background: #eee;
+export const PostsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10%;
+
+  @media screen and (max-width: 600px) {
+    padding: 1%;
+  }
+`
+
+export const PostWrapper = styled.div`
+  box-shadow: 0px 8px 24px rgba(208, 37, 0, 0.09);
+  margin: 0;
+  margin-bottom: 1.5rem;
+  /* making the edges "disappear" */
+  border-radius: 20px; 
+  background-color: ${state => state.selected ? '#D02500' : '#FFEEE7'};
+
+  transition: all 100ms ease-in-out;
+`
+
+export const Post = styled.div`
+  cursor: pointer;
+  border-radius: 8px;
+  background: #FFF;
+  padding: 1.5rem;
+  color: #6B1603;
 
   &:hover {
-    box-shadow: 2px 2px 10px 2px rgba(50, 50, 50, 0.5);
-    color: #222;
-    background: #e5e5e5;
+    color: #D02500;
   }
+`
+
+export const PostHeader = styled.div`
+  font-size: 0.75rem;
+  color: #D02500;
+`
+
+export const PostBody = styled.p`
+`
+
+
+export const Prompt = styled.div`
+  height: ${state => state.selected ? 15 : 0 }rem;
+  width: 100%;
+  background: #D02500;
+  border-radius: 0 0 20px 20px;
+  transition: all 100ms ease-in-out;
+  position: relative;
+`
+
+export const PromptTextWrapper = styled.div`
+  height: 10rem;
+  width: 100%;
+  display: ${state => state.selected ? 'flex' : 'none'};
+  align-items: center;
+  justify-content: center;
+`
+
+export const PromptText = styled.p`
+  color: #fff;
+  font-size: 4rem;
+`
+
+export const PromptControlsWrapper = styled.div`
+  position: absolute;
+  display: ${state => state.selected ? 'flex' : 'none'};
+  transition: all 100ms ease-in-out;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  bottom: 0;
+  width: 100%;
+  background-color: #6B1603;
+  border-radius: 0 0 8px 8px;
+`
+
+const ControlWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 0.9rem;
+  color: #fff;
+  max-width: 40%;
+  margin: 0 1rem;
+`
+
+export const WpmWrapper = styled(ControlWrapper)`
+  left: 0;
+  border-radius: 0 0 0 8px;
+`
+
+export const StorySelect = styled(ControlWrapper)`
+  right: 0;
+  border-radius: 0 0 8px 0;
+`
+
+export const PlayButton = styled.button`
+  position: relative;
+  border: none;
+  background-color: #fff;
+  border-radius: 999px;
+  height: 3rem;
+  width: 3rem;
+  font-size: 2.5rem;
+`
+
+export const PlayIcon = styled(BiPlay)`
+  position: absolute;
+  color: #6B1603;
+  left: 0.4rem;
+  top: 0.25rem;
+`
+
+export const PauseIcon = styled(BiPause)`
+  position: absolute;
+  color: #6B1603;
+  left: 0.25rem;
+  top: 0.25rem;
+`
+
+export const RewindArrow = styled(RiRewindFill)`
+  margin: 0 0.5rem 0 0.5rem;
+  font-size: 1.2em;
+`
+
+export const LeftArrow = styled(FaStepBackward)`
+  margin: 0;
+`
+
+export const RightArrow = styled(FaStepForward)`
+  margin: 0 0.5rem;
 `
--- src/Project/Home/index.jsx
+++ src/Project/Home/index.jsx
@@ -1,5 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Post, PostWrapper, ProjectPage, SubredditWrapper } from './Styles';
+import {
+    PostWrapper,
+    Post,
+    PostBody,
+    PostHeader,
+    PostsWrapper,
+    ProjectPage,
+    SubredditButton,
+    SubredditContainer,
+    SubredditWrapper,
+    Prompt,
+    PromptTextWrapper,
+    PromptText,
+    PromptControlsWrapper,
+    WpmWrapper,
+    PlayButton,
+    StorySelect,
+    PlayIcon,
+    PauseIcon,
+    LeftArrow,
+    RightArrow,
+    RewindArrow
+} from './Styles';
 // import React, { useEffect, useState } from 'react';
 // import { getExample, createExample } from '../../api/examples'
 
@@ -25,12 +47,85 @@ const Home = ({ user, posts, setPosts }) => {
         'AskReddit'
     ]
 
+    // subreddit state
     const [subreddits] = useState(subs)
     const [activeSub, setActiveSub] = useState(subs[0])
+
+    // select story state
+    const [selectedPost, setSelectedPost] = useState(-1)
+    const [storyCount, setStoryCount] = useState(1)
+    const [allStories, setAllStories] = useState([])
+    const [activeStory, setActiveStory] = useState([])
+    const [currStory, setCurrStory] = useState(0)
+
+    // play current story state
+    const [play, setPlay] = useState(false)
+    const [currWord, setCurrWord] = useState(0)
+    const [wpm] = useState(250)
 
     const handleActiveSub = ( event ) => {
         setActiveSub(event.target.name)
     }
+
+    const expandPost = (e, url, i) => {
+        setPlay(false)
+        setCurrWord(0)
+
+        if (selectedPost === i) {
+            setSelectedPost(-1)
+            setActiveStory([])
+        } else {
+            setSelectedPost(i)
+            setCurrStory(0)
+
+            axios.get(`${url}.json`)
+            .then(response => {
+                // console.log(response.data)
+                if (response.data[1].data.children.length > 0) {
+                    const stories = response.data[1].data.children.filter(post => post.data.author !== 'AutoModerator')
+                    setAllStories(stories)
+                    setActiveStory(stories[0].data.body.split(' '))
+                    setStoryCount(stories.length)
+                } else {
+                    setAllStories([])
+                    setActiveStory([])
+                    setStoryCount(0)
+                }
+            })
+        }
+        
+    }
+
+    const handlePrevStory = () => {
+        if (currWord > 10) {
+            setCurrWord(0)
+        }
+        else if (currStory > 0) {
+            setCurrStory(prev => prev - 1)
+            setCurrWord(0)
+        }
+    }
+
+    const handleRewindStory = () => {
+        if (currWord > 10) {
+            setCurrWord(prev => prev - 50)
+        } else {
+            setCurrWord(0)
+        }
+    }
+
+    const handleNextStory = () => {
+        if (currStory < storyCount - 1) {
+            setCurrStory(prev => prev + 1)
+            setCurrWord(0)
+        }
+    }
+
+    const wordDelay = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('foo');
+        }, 60000/wpm);
+    });
 
     useEffect(() => {
         axios({
@@ -48,31 +143,62 @@ const Home = ({ user, posts, setPosts }) => {
     // eslint-disable-next-line
     }, [activeSub])
 
+    useEffect(() => {
+        if (allStories.length > 0) {
+            setActiveStory(allStories[currStory].data.body.split(' '))
+        }
+    // eslint-disable-next-line
+    }, [currStory])
+
+    useEffect(() => {
+        if (play && currWord < activeStory.length) {
+            wordDelay
+                .then(() => {
+                    setCurrWord(prev => prev + 1)
+                })
+        }
+    // eslint-disable-next-line
+    }, [activeStory, currWord, play])
+
+    // console.log(activeStory)
     return (
         <ProjectPage>
-            <SubredditWrapper>
-                {subreddits.map(subreddit => {
-                    return <button style={{ 
-                        // padding: '1rem auto',
-                        height: '3rem',
-                        margin: '0.5rem 1rem 0.5rem 0',
-                        borderStyle: 'solid',
-                        borderWidth: '1px',
-                        borderColor: 'gray',
-                        borderRadius: '3px'
-                        }} 
-                        key={subreddit} name={subreddit} onClick={handleActiveSub}>{subreddit}</button>
+            <SubredditContainer>
+                <SubredditWrapper>
+                    {subreddits.map(subreddit => {
+                        return <SubredditButton
+                            key={subreddit} name={subreddit} onClick={handleActiveSub} selected={activeSub === subreddit}>{subreddit}</SubredditButton>
+                    })}
+                </SubredditWrapper>
+            </SubredditContainer>
+            <PostsWrapper>
+                {posts.map((post, i) => {
+                    const selected = (selectedPost === i)
+                    return (
+                        <PostWrapper key={post.data.id} selected={selected}>
+                            <Post onClick={(e) => expandPost(e, post.data.url, i)}>
+                                <PostHeader>{post.data.ups}</PostHeader>
+                                <PostBody>{post.data.title}</PostBody>
+                            </Post>
+                            <Prompt selected={selected}>
+                                <PromptTextWrapper selected={selected}>
+                                    <PromptText>{activeStory.length > 0 && activeStory[currWord]}</PromptText>
+                                </PromptTextWrapper>
+                                <PromptControlsWrapper selected={selected}>
+                                    <WpmWrapper>{activeStory.length} words at 250 wpm</WpmWrapper>
+                                    <PlayButton onClick={() => setPlay(!play)}>{play ? <PauseIcon /> : <PlayIcon />}</PlayButton>
+                                    <StorySelect>
+                                        <LeftArrow onClick={handlePrevStory} />
+                                        <RewindArrow onClick={handleRewindStory} />
+                                        {currStory + 1} of {storyCount}
+                                        <RightArrow onClick={handleNextStory} />
+                                    </StorySelect>
+                                </PromptControlsWrapper>
+                            </Prompt>
+                        </PostWrapper>
+                    )
                 })}
-            </SubredditWrapper>
-            <PostWrapper>
-                {posts.map(post => {
-                    return <Post 
-                            key={post.data.id} 
-                            to={`/story/${post.data.id}`}
-                            >{post.data.title}
-                        </Post>
-                })}
-            </PostWrapper>                         
+            </PostsWrapper>                         
         </ProjectPage>
     )
 }
